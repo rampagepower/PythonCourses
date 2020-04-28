281713ebd1c9a4ea3e5ba9b11e211309478d14c0
rampagepower <phucsi.nguyen@gmail.com>
2020-04-28 22:20:37 +0200

    Python is fun

--- PythonWorkBook/TP01/Exo-01.py
+++
@@ -1,6 +0,0 @@
-"""Create a program that displays your name and complete mailing address formatted in the manner that you would usually
-see it on the outside of an envelope. Your program does not need to read any input from the user.
-"""
-print("""Name : Rampage
-Adresse : myHomeSweetHome
-Postal Code : 9999 """)
